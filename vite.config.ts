ff7d80eadd98933c40dff47f085d9fb24eb5631d
Josue Medeiros <josuemercatus@gmail.com>
2025-09-30 14:05:33 -0300

    feat: successful build after fixing aliases and removing mocha plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import { mochaPlugins } from "@getmocha/vite-plugins";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
-  server: {
-    allowedHosts: true,
-  },
-  build: {
-    chunkSizeWarningLimit: 5000,
-  },
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+      '@': resolve(__dirname, './src'),
+      '@/react-app': resolve(__dirname, './src/react-app'),
+      '@/shared': resolve(__dirname, './src/shared')
+    }
+  }
+})
